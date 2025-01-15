b92f489694d37d616483e322c3af5eb902b846d8
Ismail <x.iml.7122@gmail.com>
2025-01-15 13:04:44 +0700

    update

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -3,8 +3,18 @@
         <h1 class="text-xl font-bold text-gray-900">Admin Panel</h1>
     </div>
     <nav class="space-y-1 px-3 flex-grow">
+        @php
+            $dashboardRoute =
+                auth()->user()->role === 'siswa'
+                    ? 'studentDashboard'
+                    : (auth()->user()->role === 'guru'
+                        ? 'teacherDashboard'
+                        : (auth()->user()->role === 'admin'
+                            ? 'adminDashboard'
+                            : 'adminDashboard'));
+        @endphp
 
-        <a href="{{ route('adminDashboard') }}"
+        <a href="{{ route($dashboardRoute) }}"
             class="flex items-center px-3 py-2 text-sm font-medium rounded-lg 
         {{ Request::routeIs($dashboardRoute) ? 'bg-gray-100 text-gray-900' : 'text-gray-600 hover:bg-gray-50' }}">
             <i data-lucide="layout-dashboard" class="w-5 h-5 mr-3"></i>
