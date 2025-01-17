9ae3d001c94d589faebb6ba70e4fde3e5ef5445e
Ismail <x.iml.7122@gmail.com>
2025-01-17 18:41:09 +0700

    remove commentify

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,5 +3,5 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\RouteServiceProvider::class,
-    Usamamuneerchaudhary\Commentify\Providers\CommentifyServiceProvider::class,
+    // Usamamuneerchaudhary\Commentify\Providers\CommentifyServiceProvider::class,
 ];
